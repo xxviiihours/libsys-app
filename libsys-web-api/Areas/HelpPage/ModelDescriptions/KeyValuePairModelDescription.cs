2eced77b632c7bc4071ff972a08cb0e96b13f60a
Kevin Macalalag <mc.kvnflrs@gmail.com>
2021-02-19 18:25:40 +0800

    Initialized Web API
    Created a web api to our project solution.

---
+++ libsys-web-api/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace libsys_web_api.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ libsys-web-api/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace libsys_web_api.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
