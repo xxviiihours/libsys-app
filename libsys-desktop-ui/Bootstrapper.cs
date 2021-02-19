d1d07006b574fc05cc4340c801b577548882b599
Kevin Macalalag <mc.kvnflrs@gmail.com>
2021-02-19 21:51:46 +0800

    Initialized Desktop UI
    Created WPF for the desktop UI

---
+++ libsys-desktop-ui/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using libsys_desktop_ui.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace libsys_desktop_ui
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
