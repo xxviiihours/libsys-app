cd96ba7a175ab88ec9b8a1c15651f764df7d48d9
Kevin Macalalag <mc.kvnflrs@gmail.com>
2021-02-27 16:36:59 +0800

    Refactored libsys web api
    Changed Name to libsys-api

---
+++ libsys-api/App_Start/AuthorizationOperationFilter.cs
@@ -0,0 +1,29 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace libsys_api.App_Start
+{
+    public class AuthorizationOperationFilter : IOperationFilter
+    {
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            if(operation.parameters == null)
+            {
+                operation.parameters = new List<Parameter>();
+            }
+
+            operation.parameters.Add(new Parameter
+            {
+                name = "Authorization",
+                description = "Access Token",
+                @in = "header",
+                @default = "Bearer ",
+                type = "string"
+            });
+        }
+    }
+}
---
+++ libsys-api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="libsys_api.WebApiApplication" Language="C#" %>
