535ead9c2b29f25eb422b9dfadacc6c330b99ca0
xxviiihours <mc.kvnflrs@gmail.com>
2021-03-07 18:51:54 +0800

    Added CORS reference
    Added CORS to support
    Front end web application

--- libsys-api/App_Start/WebApiConfig.cs
+++ libsys-api/App_Start/WebApiConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
 
@@ -19,6 +20,9 @@ namespace libsys_api
 
             // Web API routes
             config.MapHttpAttributeRoutes();
+
+            var corsAttr = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(corsAttr);
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
